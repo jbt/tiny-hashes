62748bbe8e6753bff7f44fd10f9a032f4e04071e
JT <jt@gosquared.com>
2012-04-24 21:40:32 +0100

    Been a while since I updated these. Whitespace, plus shorten SHA256.

--- md5.js
+++ md5.js
@@ -1,89 +1,82 @@
 md5 = function(sixteen,ffff){
 
-	var k = [],i=0;
+  var k = [],i=0;
 
-	for(;i<64;){
-		k[i] = 0|(Math.abs(Math.sin(++i)) * 4294967296);
-	}
+  for(;i<64;){
+    k[i] = 0|(Math.abs(Math.sin(++i)) * 4294967296);
+  }
 
-	function rhex(num){
-		var str = "",j=0;
-		for(; j <4;)	str += (256|(num >> (j++ * 8)) & 0x1FF).toString(sixteen).slice(1);
-		return str;
-	}
+  function rhex(num){
+    var str = "",j=0;
+    for(;j<4;)str += (256|(num >> (j++ * 8)) & 0x1FF).toString(sixteen).slice(1);
+    return str;
+  }
 
-	function str2blks_MD5(str){
-		var	blks = [],i=0,j=str.length;
-		for(;i<=j;)	blks[i >> 2] |= (str.charCodeAt(i)||128) << ((i++ % 4) * 8);
-		blks[((j+ 8) >> 6)*sixteen+14] = j * 8;
-		return blks;
-	}
+  function str2blks_MD5(str){
+    var blks = [],i=0,j=str.length;
+    for(;i<=j;) blks[i >> 2] |= (str.charCodeAt(i)||128) << ((i++ % 4) * 8);
+    blks[((j+ 8) >> 6)*sixteen+14] = j * 8;
+    return blks;
+  }
 
-	function add(x, y){
-		var msw = (x >> sixteen) + (y >> sixteen) + ((y=(x & ffff) + (y & ffff)) >> sixteen);
-		return (msw << sixteen) | (y & ffff);
-	}
-	function rol(num, cnt){
-		return (num << cnt)|(num >>> (32 - cnt));
-	}
+  function add(x, y){
+    var msw = (x >> sixteen) + (y >> sixteen) + ((y=(x & ffff) + (y & ffff)) >> sixteen);
+    return (msw << sixteen) | (y & ffff);
+  }
+  function rol(num, cnt){
+    return (num << cnt)|(num >>> (32 - cnt));
+  }
 
 
-	var calcMD5 = function(str){
-		var a,b,c,d,temp,f,i=0,j,m,
-		x = str2blks_MD5(unescape(encodeURIComponent(str))),
-		h0 =  1732584193,
-		h3 = 271733878,
-		h2 = ~h0,
-		h1 =  ~h3;
+  var calcMD5 = function(str){
+    var a,b,c,d,temp,f,i=0,j,m,
+        x = str2blks_MD5(unescape(encodeURIComponent(str))),
+        h0 = 1732584193,
+        h3 = 271733878,
+        h2 = ~h0,
+        h1 = ~h3;
 
-		for(; i < x.length; i += sixteen)
-		{
-
-			a = h0;
-			b = h1;
-			c = h2;
-			d = h3;
-			for(j=0;j<64;a=temp){
-				f = [
-					((b&c)|((~b)&d)),
-					((d&b)|((~d)&c)),
-					(b^c^d),
-					(c^(b|(~d)))
-				][m=j>>4];
-				temp = d;
-				d = c;
-				c = b;
-				b = add(
-						b , 
-						rol(add(
-								add(
-									a ,
-									f
-								),
-								add(
-									k[j] ,
-									x[[
-										j,
-										(5*j+1),
-										(3*j+5),
-										(7*j)
-									][m]%sixteen+i]
-								)
-						),[
-							7, 12, 17, 22,
-							5,  9, 14, 20,
-							4, 11, sixteen, 23,
-							6, 10, 15, 21
-						][4*m+j++%4]
-					));
-			}
-			h0 = add(h0 , a);
-			h1 = add(h1 , b);
-			h2 = add(h2 , c);
-			h3 = add(h3 , d);
-
-		}
-		return rhex(h0) + rhex(h1) + rhex(h2) + rhex(h3);
-	};
-	return calcMD5;
+    for(; i < x.length; i += sixteen){
+      a = h0;
+      b = h1;
+      c = h2;
+      d = h3;
+      for(j=0;j<64;a=temp){
+        f = [
+          ((b&c)|((~b)&d)),
+          ((d&b)|((~d)&c)),
+          (b^c^d),
+          (c^(b|(~d)))
+        ][m=j>>4];
+        temp = d;
+        d = c;
+        c = b;
+        b = add( b ,
+          rol(add(
+            add( a, f ),
+            add(
+              k[j],
+              x[[
+                j,
+                (5*j+1),
+                (3*j+5),
+                (7*j)
+              ][m]%sixteen+i]
+            )
+          ),[
+            7, 12, 17, 22,
+            5,  9, 14, 20,
+            4, 11, sixteen, 23,
+            6, 10, 15, 21
+          ][4*m+j++%4]
+        ));
+      }
+      h0 = add(h0 , a);
+      h1 = add(h1 , b);
+      h2 = add(h2 , c);
+      h3 = add(h3 , d);
+    }
+    return rhex(h0) + rhex(h1) + rhex(h2) + rhex(h3);
+  };
+  return calcMD5;
 }(16,0xffff);
--- sha1.js
+++ sha1.js
@@ -1,77 +1,74 @@
 sha1 = function(bigNum){
-	function rotate_left(n, s) {
-		return  (n << s) | (n >>> (32 - s));
-	}
+  function rotate_left(n, s) {
+    return (n << s) | (n >>> (32 - s));
+  }
 
-	function cvt_hex(val,str,i) {
-		str = "";
-		for (i = 8; i--;) {
-			str += ((val >>> (i * 4)) & 15).toString(16);
-		}
-		return str;
-	}
+  function cvt_hex(val,str,i) {
+    str = "";
+    for (i = 8; i--;) {
+      str += ((val >>> (i * 4)) & 15).toString(16);
+    }
+    return str;
+  }
 
-	function sha1 (str1) {
-		
+  function sha1 (str1) {
+    var blockstart=0,
+        i = 0,
+        W = [],
+        H0 = 0x67452301,
+        H1 = 0xEFCDAB89,
+        H2 = 0x98BADCFE,
+        H3 = 0x10325476,
+        H4 = 0xC3D2E1F0,
+        A, B, C, D, E, F, G, H,
+        temp,
+        word_array = [],
+        temp2,
+        str = unescape(encodeURIComponent(str1)),
+        str_len = str.length;
 
-		var blockstart=0;
-		var i = 0;
-		var W = [];
-		var H0 = 0x67452301;
-		var H1 = 0xEFCDAB89;
-		var H2 = 0x98BADCFE;
-		var H3 = 0x10325476;
-		var H4 = 0xC3D2E1F0;
-		var A, B, C, D, E, F, G, H;
-		var temp;
-		var word_array = [];
-		var temp2;
+    for (; i<=str_len;){
+      word_array[i>>2] |= (str.charCodeAt(i)||128)<<(8*(3-i++%4));
+    }
+    temp2 = (str_len>>6)*16+14;
+    word_array[temp2++] = str_len>>>29;
+    word_array[temp2++] = (str_len << 3) & bigNum
+    for (; blockstart < temp2; blockstart += 16) {
+      for (i = -1; ++i < 80;) {
+        temp = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16]
+        W[i] = (i<16) ? ~~word_array[blockstart + i] : (rotate_left(temp, 1));
+      }
 
-		var str = unescape(encodeURIComponent(str1));
-		var str_len = str.length;
+      A = H0;
+      B = H1;
+      C = H2;
+      D = H3;
+      E = H4;
 
-		for (; i<=str_len;){
-			word_array[i>>2] |= (str.charCodeAt(i)||128)<<(8*(3-i++%4));
-		}
-		temp2 = (str_len>>6)*16+14;
-		word_array[temp2++] = str_len>>>29;
-		word_array[temp2++] = (str_len << 3) & bigNum
-		for (; blockstart < temp2; blockstart += 16) {
-			for (i = -1; ++i < 80;) {
-				temp = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16]
-				W[i] = (i<16) ? ~~word_array[blockstart + i] : (rotate_left(temp, 1));
-			}
+      for (i = 0; i < 80; ) {
+        G = rotate_left(A,5);
+        H = E + W[i] + 1518500249;
+        temp = [
+          G + ((B & C) | (~B & D)) + H,
+          F = G + (B ^ C ^ D) + H + 341275144,
+          G + ((B & C) | (B & D) | (C & D)) + H + 882459459,
+          F + 1535694389
+        ][0|(i++/20)] & bigNum;
+        E = D;
+        D = C;
+        C = rotate_left(B, 30);
+        B = A;
+        A = temp;
+      }
 
-			A = H0;
-			B = H1;
-			C = H2;
-			D = H3;
-			E = H4;
+      H0 = (H0 + A) & bigNum;
+      H1 = (H1 + B) & bigNum;
+      H2 = (H2 + C) & bigNum;
+      H3 = (H3 + D) & bigNum;
+      H4 = (H4 + E) & bigNum;
+    }
 
-			for (i = 0; i < 80; ) {
-				G = rotate_left(A,5);
-				H = E + W[i] + 1518500249;
-				temp = [
-					G + ((B & C) | (~B & D)) + H,
-					F = G + (B ^ C ^ D) + H + 341275144,
-					G + ((B & C) | (B & D) | (C & D)) + H + 882459459,
-					F + 1535694389
-				][0|(i++/20)] & bigNum;
-				E = D;
-				D = C;
-				C = rotate_left(B, 30);
-				B = A;
-				A = temp;
-			}
-
-			H0 = (H0 + A) & bigNum;
-			H1 = (H1 + B) & bigNum;
-			H2 = (H2 + C) & bigNum;
-			H3 = (H3 + D) & bigNum;
-			H4 = (H4 + E) & bigNum;
-		}
-
-		return cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-	}
-	return sha1
+    return cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+  }
+  return sha1
 }(0x0ffffffff);
