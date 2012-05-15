578a1d3a383448e865d1c969f732e98f4c6e1fd8
JT <jt@gosquared.com>
2012-05-15 17:56:46 +0100

    SHA1 now 678 bytes

--- sha1.js
+++ sha1.js
@@ -7,13 +7,8 @@ sha1 = function(bigNum){
     var blockstart=0,
         i = 0,
         W = [],
-        H0 = 0x67452301,
-        H1 = 0xEFCDAB89,
-        H2 = 0x98BADCFE,
-        H3 = 0x10325476,
-        H4 = 0xC3D2E1F0,
-        A, B, C, D, E, F, G,
-        temp,
+        H = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0],
+        A, B, C, D, F, G,
         word_array = [],
         temp2,
         str = unescape(encodeURIComponent(str1)),
@@ -25,41 +20,29 @@ sha1 = function(bigNum){
     word_array[temp2 = (str_len>>6)*16+15] = str_len>>>29;
     word_array[temp2++] = (str_len << 3) & bigNum;
     for (; blockstart < temp2; blockstart += 16) {
-      for (i = -1; ++i < 80;) {
-        temp = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
-        W[i] = (i<16) ? ~~word_array[blockstart + i] : (rotate_left(temp, 1));
+      for (i = 0; i < 80; i++) {
+        G = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
+        W[i] = (i<16) ? ~~word_array[blockstart + i] : (rotate_left(G, 1));
       }
 
-      A = H0;
-      B = H1;
-      C = H2;
-      D = H3;
-      E = H4;
+      A = H.slice(i=0);
 
-      for (i = 0; i < 80; ) {
-        G = rotate_left(A,5) + E + W[i] + 1518500249;
-        temp = [
-          G + ((B & C) | (~B & D)),
+      for (; i < 80; ) {
+        G = rotate_left(A[0],5) + A[4] + W[i] + 1518500249;
+        G = [
+          G + (((B=A[1]) & (C=A[2])) | (~B & (D=A[3]))),
           F = G + (B ^ C ^ D) + 341275144,
           G + ((B & C) | (B & D) | (C & D)) + 882459459,
           F + 1535694389
         ][0|(i++/20)] & bigNum;
-        E = D;
-        D = C;
-        C = rotate_left(B, 30);
-        B = A;
-        A = temp;
+
+        A = [G, A[0], rotate_left(B, 30), C, D];
       }
 
-      H0 = (H0 + A) & bigNum;
-      H1 = (H1 + B) & bigNum;
-      H2 = (H2 + C) & bigNum;
-      H3 = (H3 + D) & bigNum;
-      H4 = (H4 + E) & bigNum;
+      for(i=5;i;) H[--i] = (H[i] + A[i]) & bigNum;
     }
 
-    str1='';
-    for(i=0;i<40;)str1 += ((([H0,H1,H2,H3,H4][i>>3]) >>> (7-i++%8)*4) & 15).toString(16);
+    for(str1='';i<40;)str1 += (((H[i>>3]) >>> (7-i++%8)*4) & 15).toString(16);
     return str1;
   }
   return sha1;
