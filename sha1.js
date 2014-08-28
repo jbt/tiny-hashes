d6b7736eb4501079ef912cafa3a923f9d5f31c4e
JT <jt@gosquared.com>
2014-08-28 09:03:33 +0100

    Compile @heavensrevenge's changes (yay bytes!); also add some whitespace
    I've decided I like whitespace now.

--- sha1.js
+++ sha1.js
@@ -1,5 +1,6 @@
 sha1 = function (str1){
-  for (var blockstart=0,
+  for (
+    var blockstart = 0,
       i = 0,
       W = [],
       H = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0],
@@ -9,28 +10,29 @@ sha1 = function (str1){
       s = unescape(encodeURI(str1)),
       str_len = s.length;
 
-    i<=str_len;){
-    word_array[i>>2] |= (s.charCodeAt(i)||128)<<(8*(3-i++%4));
+    i <= str_len;
+  ){
+    word_array[i >> 2] |= (s.charCodeAt(i)||128) << (8 * (3 - i++ % 4));
   }
-  word_array[temp2 = ((str_len+8)>>6<<4)+15] = str_len<<3;
+  word_array[temp2 = ((str_len + 8) >> 6 << 4) + 15] = str_len << 3;
 
   for (; blockstart <= temp2; blockstart += 16) {
     A = H; i = 0;
 
     for (; i < 80;
       A = [[
-        (G = ((s=A[0])<<5|s>>>27) + A[4] + (W[i] = (i<16) ? ~~word_array[blockstart + i] : G<<1|G>>>31) + 1518500249) + ((B=A[1]) & (C=A[2]) | ~B & (D=A[3])),
+        (G = ((s = A[0]) << 5 | s >>> 27) + A[4] + (W[i] = (i<16) ? ~~word_array[blockstart + i] : G << 1 | G >>> 31) + 1518500249) + ((B = A[1]) & (C = A[2]) | ~B & (D = A[3])),
         F = G + (B ^ C ^ D) + 341275144,
         G + (B & C | B & D | C & D) + 882459459,
         F + 1535694389
-      ][0|((i++) / 20)] | 0, s, B<<30|B>>>2, C, D]
+      ][0|((i++) / 20)] | 0, s, B << 30 | B >>> 2, C, D]
     ) {
       G = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16];
     }
 
-    for(i=5;i;) H[--i] = H[i] + A[i] | 0;
+    for(i = 5; i; ) H[--i] = H[i] + A[i] | 0;
   }
 
-  for(str1='';i<40;)str1 += (H[i>>3] >> (7-i++%8)*4 & 15).toString(16);
+  for(str1 = ''; i < 40; )str1 += (H[i >> 3] >> (7 - i++ % 8) * 4 & 15).toString(16);
   return str1;
 };
